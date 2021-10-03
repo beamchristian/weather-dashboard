5244171624e1ba168f7569fbe6dcb908edd1bad3
beamchristian <beamchristian@yahoo.com>
2021-10-03 13:06:49 -0400

    chained apis and displaying all current weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,21 @@ const cityContainerEl = document.querySelector('#city-container');
 const cityTitleEl = document.querySelector('#city-search-title');
 const cityTemp = document.querySelector('#city-search-temp');
 const cityWind = document.querySelector('#city-search-wind');
+const cityHumidity = document.querySelector('#city-search-humidity');
+const cityUvi = document.querySelector('#city-search-uvi');
+const uviText = document.querySelector('#uvi-text');
 
-const getUserForcast = function (city) {
-  // format the OpenWeather One Call API
-  const apiUrl =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
-    cityInputEl.value +
-    '&units=imperial' +
-    '&appid=01cec3e443f278224b33dd45c5cbe37c';
+// current date variables
+let today = new Date();
+let month = today.getMonth() + 1;
+let year = today.getFullYear();
+let date = today.getDate();
+let currentDate = `(${month}/${date}/${year})`;
 
-  // make a request to the url
-  fetch(apiUrl)
-    .then(function (response) {
-      // request was succesful
-      if (response.ok) {
-        response.json().then(function (data) {
-          console.log(data);
-          displayWeatherInfo(data, city);
-        });
-      } else {
-        alert('City Not Found');
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to Api');
-    });
+// display icon function
+const displayIcon = function (src) {
+  let img = document.createElement('img');
+  img.src = src;
 };
 
 const formSubmitHandler = function (event) {
@@ -46,20 +36,76 @@ const formSubmitHandler = function (event) {
   }
 };
 
-const displayIcon = function (src) {
-  let img = document.createElement('img');
-  img.src = src;
+const getUserForcast = function (city) {
+  // put OpenWeather, Weather API into a variable within the function
+  const weatherApiUrl =
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    cityInputEl.value +
+    '&units=imperial' +
+    '&appid=01cec3e443f278224b33dd45c5cbe37c';
+
+  // call weather Api
+  fetch(weatherApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      displayWeatherInfo(data);
+      const latCoord = data.coord.lat;
+      const lonCoord = data.coord.lon;
+      // call OneCall Api
+      return fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?q=' +
+          '&units=imperial' +
+          '&lat=' +
+          latCoord +
+          '&lon=' +
+          lonCoord +
+          '&appid=01cec3e443f278224b33dd45c5cbe37c'
+      );
+    })
+    .then(response => response.json())
+    .then(data => displayOnecall(data));
+};
+
+const removeCurrentUviClass = function () {
+  uviText.classList.remove('low');
+  uviText.classList.remove('medium');
+  uviText.classList.remove('high');
+  uviText.classList.remove('very-high');
+};
+
+const displayOnecall = function (data) {
+  console.log(data);
+  const displayUvi = function () {
+    let uviValue = data.current.uvi;
+    if (uviValue < 2) {
+      removeCurrentUviClass();
+      uviText.classList.add('low');
+    } else if (uviValue > 2 && uviValue < 5) {
+      removeCurrentUviClass();
+      uviText.classList.add('medium');
+    } else if (uviValue > 5 && uviValue < 7) {
+      removeCurrentUviClass();
+      uviText.classList.add('high');
+    } else if (uviValue > 7) {
+      removeCurrentUviClass();
+      uviText.classList.add('very-high');
+    }
+    uviText.innerHTML = `${uviValue}`;
+  };
+  displayUvi();
 };
 
 const displayWeatherInfo = function (data) {
   let nameValue = data.name;
-  let weatherIcon = data.weather[0].icon;
-  let tempValue = data['main']['temp'];
+  let tempValue = data.main.temp;
+  let windValue = data.wind.speed;
+  let humidityValue = data.main.humidity;
 
-  cityTitleEl.innerHTML = nameValue;
-  cityTemp.innerHTML = tempValue + '°F';
+  cityTitleEl.innerHTML = `${nameValue} ${currentDate}`;
+  cityTemp.innerHTML = `Temp: ${tempValue}°F`;
+  cityWind.innerHTML = `Wind: ${windValue} MPH`;
+  cityHumidity.innerHTML = `Humidity: ${humidityValue} %`;
 };
-
-// const searchSubmitHandler = function (event) {};
 
 userSearchEl.addEventListener('submit', formSubmitHandler);
